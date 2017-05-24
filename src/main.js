23491612bd6cd77cc6da74cb1178c3accb14ea66
程帅 <tsl-chengshuai@tasly.com>
2017-05-24 15:42:52 +0800

    添加权限控制，没有权限，router直接跳转到登录

--- src/main.js
+++ src/main.js
@@ -24,23 +24,62 @@ const routes = {
         component: Index,
         subRoutes: {
             '/home': {
-                component: TabBarHome
+                component: TabBarHome,
+                name:'home'
             },
             '/list': {
-                component: TabBarList
+                component: TabBarList,
+                name:'list'
             },
             '/cart': {
-                component: TabBarCart
+                component: TabBarCart,
+                meta: {
+                    requireAuth: true,
+                },
+                name:'cart'
             },
             '/user': {
-                component: TabBarUser
+                component: TabBarUser,
+                meta: {
+                    requireAuth: true,
+                },
+                name:'user'
             }
         }
     },
     '/login': {
-        component: Login
+        component: Login,
+        name:'login'
     },
 };
+
+const beforeEach = (t) => {
+    // 判断是否需要权限
+    if(t.to.meta && t.to.meta.requireAuth){
+        //TODO 在session中查看token
+        t.redirect({
+            path:'/login'
+        })
+    }else{
+        t.next()
+    }
+}
+/*const afterEach = (t) => {
+    const to = t.to.path
+    const from = t.from.path
+    // [Custom Business] Never use history scrollTop when '/' => '/home'
+    if (from == '/' && to == '/home') return
+
+    const h = sess.get(t.to.path)
+    if (h && h.scrollTop) {
+        Vue.nextTick(() => {
+            Vonic.app.pageContentScrollTop(h.scrollTop)
+        })
+    }
+}*/
+
+Vonic.app.setConfig('beforeEach',beforeEach)
+// Vonic.app.setConfig('afterEach', afterEach)
 
 Vue.use(Vonic.app, {routes: routes, defaultRouteUrl: '/index/home'});
 
